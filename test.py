e414c768b1ca6963efd388bd9e8977ccc0b79ca3
i0Ek3 <kno30826@gmail.com>
2022-02-07 13:20:56 +0800

    test: Test test.py

--- test.py
+++ test.py
@@ -28,15 +28,13 @@ def main():
     solidot = get_link_info("https://www.solidot.org/index.rss", 5)
     fmt = '%Y-%m-%d %H:%M:%S %Z%z'
     msg = "Auto update by GitHub Action"
-    insert_info = "+++BEGIN+++\n\n## " + msg + "(" + "Update time: " + datetime.fromtimestamp(int(time.time()),pytz.timezone('Asia/Shanghai')).strftime('%Y-%m-%d %H:%M:%S') + ")" + "\n" + solidot + "\n+++END+++"
+    insert_info = "# " + msg + "\nUpdate time: " + datetime.fromtimestamp(int(time.time()),pytz.timezone('Asia/Shanghai')).strftime('%Y-%m-%d %H:%M:%S') + "\n" + solidot + "\n"
     filename = "news.md"
 
     with open (os.path.join(os.getcwd(), filename), 'r', encoding='utf-8') as f:
         news_content = f.read()
 
-    new_content = re.sub(r'+++BEGIN+++(.|\n)*+++END+++', insert_info, news_content)
-
     with open (os.path.join(os.getcwd(), filename), 'w', encoding='utf-8') as f:
-        f.write(new_content)
+        f.write(insert_info)
 
 main()
